fcf890e83292ebe06dd4f27c6da06c448af76d00
Tim Borisenko <Tborisen>
2019-11-17 01:42:49 -0800

    added countdown

--- Accelerometer/app/src/main/java/com/example/accelerometer/FallDetection.java
+++ Accelerometer/app/src/main/java/com/example/accelerometer/FallDetection.java
@@ -6,11 +6,22 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class FallDetection implements SensorEventListener {
 
@@ -18,6 +29,12 @@ public class FallDetection implements SensorEventListener {
     Sensor sns;
     TextView txt;
     Context testContext;
+
+    int interval;
+    Timer timer;
+    boolean notified = false;
+
+    String FALLMSG = "Fall Detected, Contacting Emergency Contact in ";
 
     public FallDetection(Context mainClass) {
         testContext = mainClass;
@@ -29,6 +46,7 @@ public class FallDetection implements SensorEventListener {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         float xSquared = sensorEvent.values[0] * sensorEvent.values[0];
@@ -36,9 +54,30 @@ public class FallDetection implements SensorEventListener {
         float zSquared = sensorEvent.values[2] * sensorEvent.values[2];
         double xyzSRoot = Math.sqrt(xSquared + ySquared + zSquared);
 
-        if(xyzSRoot > 15) {
-            //Toast.makeText(testContext, "Detected graph spike", Toast.LENGTH_SHORT).show();
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        interval = 3;
+
+
+        if(xyzSRoot > 10 && !notified) {
+
+            Toast.makeText(testContext, FALLMSG, Toast.LENGTH_SHORT).show();
+
+            while (interval > 0){
+                Toast.makeText(testContext, String.valueOf(interval), Toast.LENGTH_SHORT).show();
+               for (double i = 0; i < 1000000*1000000; i++){ }
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                interval = interval - 1;
+            }
+
             this.sendAlert();
+            notified = true;
+            return;
         }
     }
 
@@ -50,7 +89,7 @@ public class FallDetection implements SensorEventListener {
     private void sendAlert(){
 
         String phoneNumber = "4256266630";
-        String smsMessage = "Fall Detected! Kenzo Bannang fell and is in need of emergency assistance!";
+        String smsMessage = "Fall Detected!";
 
         SmsManager sms = SmsManager.getDefault();
         try {
@@ -62,3 +101,16 @@ public class FallDetection implements SensorEventListener {
         }
     }
 }
+
+
+//            timer.scheduleAtFixedRate(new TimerTask() {
+//                public void run() {
+//                    Looper.prepare();
+//                    Handler mHandler = new Handler() {
+//                        public void handleMessage(Message msg) {
+//                            Toast.makeText(testContext, Integer.toString(setInterval()), Toast.LENGTH_SHORT).show();
+//                        }
+//                    };
+//                    Looper.loop();
+//                }
+//            }, delay, period);
