1ed3efcfc78775846c73f30d87bfd31b9f85b3c3
kenzobanaag <kenzobanaag@ymail.com>
2019-12-08 18:44:50 -0800

    Yea

---
+++ EyeBallinApp/app/src/main/java/com/example/eyeballinapp/MapStuff/Graph/Step.java
@@ -0,0 +1,164 @@
+package com.example.eyeballinapp.MapStuff.Graph;
+
+import android.location.Location;
+
+import com.example.eyeballinapp.MapStuff.Graph.EBVector;
+import com.example.eyeballinapp.MapStuff.Graph.MapNode;
+import com.example.eyeballinapp.MapStuff.Location.CustomLocation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Step {
+    private MapNode node;
+    private EBVector vector;
+    private String direction; // the direction the user needs to go for their next step
+    private boolean hasZComponent;
+
+    public Step(MapNode node, double distanceX, double distanceY) {
+        this.node = node;
+        this.vector = new EBVector(distanceX, distanceY);
+    }
+
+    public Step(MapNode node, Location loc) {
+        CustomLocation nodeLocation = (CustomLocation) node.getLocation();
+        CustomLocation l = (CustomLocation) loc;
+        double y = nodeLocation.getPositionY() - l.getPositionY();
+        double x = nodeLocation.getPositionX() - l.getPositionX();
+        int z = nodeLocation.getFloorNum() - l.getFloorNum();
+
+        if (z == 0)
+            hasZComponent = false;
+        else
+            hasZComponent = true;
+
+        this.node = node;
+        this.vector = new EBVector(x, y);
+    }
+
+    public void updateVector(EBVector v) {
+        this.vector = v;
+    }
+
+    public void updateVector(Location loc) {
+        CustomLocation nodeLocation = (CustomLocation) node.getLocation();
+        CustomLocation l = (CustomLocation) loc;
+        double y = nodeLocation.getPositionY() - l.getPositionY();
+        double x = nodeLocation.getPositionX() - l.getPositionX();
+        int z = nodeLocation.getFloorNum() - l.getFloorNum();
+
+        if (z == 0)
+            hasZComponent = false;
+        else
+            hasZComponent = true;
+
+        this.vector = new EBVector(x, y);
+    }
+
+    // determine if vector is 0 without calculation.
+    public boolean hasZComponent() {
+        return hasZComponent;
+    }
+
+    // determine if there is a direction with a 0 component. (for finding near-right turns)
+    public boolean has0Component() {
+        if (vector.getX() == 0 || vector.getY() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    // get the magnitude of the distance to the next node.
+    public double getDistance() {
+        return vector.getMagnitude();
+    }
+
+    public EBVector getVector() {
+        return vector;
+    }
+
+    public MapNode getNode() {
+        return node;
+    }
+
+    /**
+     * https://i.imgur.com/QHf5w6n.jpg drawing explanation of the breakup of directions.
+     * NOTE: The graph drawn has left and right mixed up! they are opposites!
+     * @param dir a string representing the direction that is either 'forward', 'left', 'right', 'back' relative to the coordinate plane where positive Y is up and positive X is right.
+     */
+    public String setUserDirection(String dir) {
+
+        // cannot give them a direction to turn if they are in the elevator
+        if (this.hasZComponent()) {
+            direction = "elevator";
+            return direction;
+        }
+
+        //need to add directions to give for when you are in an elevator. Just tell them what floor to go to.
+
+        EBVector directionVector;
+        double vx = vector.getX();
+        double vy = vector.getY();
+
+        // we need to rotate our direction vector based on the direction the user is facing so that our orientation is the same.
+        // To do this, we use a rotation matrix for 90, 180, and 270 degrees based on which direction they are facing.
+        // rotations for exact 90 degree angles are simply transformations of the signs of the components.
+        if (dir.equals("forward")) { //positive Y direction
+            directionVector = new EBVector(vy, -vx); // transformation: <y, -x>
+        } else if (dir.equals("left")) { // negative x direction
+            directionVector = new EBVector(-vx, -vy); // transformation: <-x, -y>
+        } else if (dir.equals("back")) { //negative y direction
+            directionVector = new EBVector(-vy, vx); // transformation: <-y, x>
+        } else if (dir.equals("right")) { // we do not need to rotate if they are facing in the positive X direction since their vectors already line up with our representation of directions.
+            directionVector = vector;
+        } else {
+            throw new IndexOutOfBoundsException("expected string in the form 'forward', 'left', 'right', 'back' and got" + dir);
+        }
+
+        // calculate the unit vector so we can determine which whay to tell the user to go.
+        // adding the two vectors will give us one pointing
+        EBVector turningDirection = directionVector.getUnitVector();
+
+        // pull out all the values we care about
+        double x = turningDirection.getX();
+        double y = turningDirection.getY();
+
+        //if we split the unit circle based on the y component, we can make a more clear if-else chain
+        if (y >= EBVector.twentyThreePiOver12y) { // vector is pointing between F and B.
+            if (x > EBVector.piOver12x) {
+                direction = "forward";
+            } else if (x > EBVector.fivePiOver12x) {
+                direction = "slight_left";
+            } else if (x > EBVector.sevenPiOver12x) {
+                direction = "left";
+            } else if (x > EBVector.elevenPiOver12x) {
+                direction = "120degrees_left";
+            } else {
+                direction = "backward";
+            }
+        } else { // Vector is pointing between 120R and SR
+            if (x > EBVector.nineteenPiOver12x) {
+                direction = "slight_right";
+            } else if (x > EBVector.seventeenPiOver12x) {
+                direction = "right";
+            } else {
+                direction = "120degrees_right";
+            }
+        }
+
+        return direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    private boolean ghettoRequiredNodesList(String name) {
+        Pattern p = Pattern.compile("(Outside Hallway|Water Fountain|Outside.*61)"); // anything that contains outside hallway, water fountain, or outside the seminar rooms is required.
+        Matcher m = p.matcher(name);
+        if (m.matches())
+            return true;
+        else
+            return false;
+    }
+}
---
+++ EyeBallinApp/app/src/main/java/com/example/eyeballinapp/MapStuff/Navigation/Navigation.java
@@ -0,0 +1,110 @@
+// This class will interact with the voice classes to gather user input and create a navigation plan.
+// It will also generate the text that will be spoken to navigate the user to their location
+// This class will also create a location listener that will be used to navigate the user
+// For testing
+
+package com.example.eyeballinapp.MapStuff.Navigation;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.eyeballinapp.EventBus.OnButtonClickedMessage;
+import com.example.eyeballinapp.MapStuff.Graph.Route;
+import com.example.eyeballinapp.MapStuff.Graph.Step;
+import com.example.eyeballinapp.MapStuff.Location.CustomLocation;
+import com.example.eyeballinapp.SpeechStuff.SpeakActivity;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+public class Navigation {
+    EyeBallinMap map;
+    Context mContext;
+    CustomLocation userLocation;
+
+    private String lastLocation = "forward";
+    private String currentLocation = "forward";
+
+    private SpeakActivity say;
+
+    private int stepLength = 1;
+    Route steps;
+
+    public Navigation(Context context, String destination) {
+        mContext = context;
+        map = new EyeBallinMap(mContext);
+        //starting point
+        userLocation = new CustomLocation(0, 0, 1);
+        map.setDestination(destination);
+        map.updateUser(userLocation);
+        steps = map.calculateRoute();
+        //setDirection("highest");
+        say = new SpeakActivity(mContext, "");
+        //Toast.makeText(context, "Dest: " + destination, Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigate(String direction) {
+        switch (direction) {
+            //do a post here using eventbus
+            case "forward":
+                move(userLocation.getPositionX(), userLocation.getPositionY() + stepLength, userLocation.getFloorNum(), "forward");
+                lastLocation = "forward";
+                break;
+            case "back":
+                move(userLocation.getPositionX(), userLocation.getPositionY() - stepLength, userLocation.getFloorNum(), "back");
+                lastLocation = "back";
+                break;
+            case "left":
+                move(userLocation.getPositionX() - stepLength, userLocation.getPositionY(), userLocation.getFloorNum(), "left");
+                lastLocation = "left";
+                break;
+            case "right":
+                move(userLocation.getPositionX() + stepLength, userLocation.getPositionY(), userLocation.getFloorNum(),"right");
+                lastLocation = "right";
+                break;
+            case "up":
+                if (userLocation.getFloorNum() != 4) {
+                    move(userLocation.getPositionX(), userLocation.getPositionY(), userLocation.getFloorNum() + 1, "up");
+                    //say.speak("Going up, you are at floor" + userLocation.getFloorNum());
+                    //lastLocation = "up";
+                }
+                break;
+            case "down":
+                if (userLocation.getFloorNum() != 1) {
+                    move(userLocation.getPositionX(), userLocation.getPositionY(), userLocation.getFloorNum() - 1, "down");
+                    say.speak( "Going down, you are at floor" + userLocation.getFloorNum());
+                    //lastLocation = "down";
+                }
+                break;
+            default:
+        }
+        EventBus.getDefault().post(new OnButtonClickedMessage("UPDATE"));
+    }
+
+    public List<Step> getStepList() {
+        return steps.getStepList();
+    }
+
+    private void move(double x, double y, int floor, String direction) {
+        userLocation.setLocation(x, y, floor);
+        map.updateUser(userLocation);
+        int stepLength = steps.getStepList().size();
+        steps = map.calculateRoute();
+
+        //step size decreased so going on the right path.
+        if(stepLength != steps.getStepList().size() && !direction.equals("up")) {
+            say.speak( steps.getVoiceDirections(direction));
+            EventBus.getDefault().post(new OnButtonClickedMessage("CHANGED", steps.getGeneralWalkingDirection(direction)));
+        }
+
+        if(stepLength != steps.getStepList().size() && direction.equals("up")) {
+            say.speak( steps.getVoiceDirections("left"));
+        }
+
+        if(steps.getStepList().size() == 1 && steps.getStep(0).getDistance() == 0) {
+            say.speak( "Yay! You have reached your destination");
+            EventBus.getDefault().post(new OnButtonClickedMessage("FINISHED"));
+        }
+    }
+}
